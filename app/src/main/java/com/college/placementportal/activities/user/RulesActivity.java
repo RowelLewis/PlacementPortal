1d38a2f4664851c559a55f7d686f77579ad2a929
RowelLewis <60008336+RowelLewis@users.noreply.github.com>
2021-12-21 22:27:16 +0530

    fixed the app launch failure

--- app/src/main/java/com/college/placementportal/activities/user/RulesActivity.java
+++ app/src/main/java/com/college/placementportal/activities/user/RulesActivity.java
@@ -1,16 +1,24 @@
 package com.college.placementportal.activities.user;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.text.Html;
 
 import com.college.placementportal.R;
+import com.college.placementportal.databinding.ActivityRulesBinding;
 
 public class RulesActivity extends AppCompatActivity {
 
+    ActivityRulesBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_rules);
+
+        binding.t1.setText(Html.fromHtml(("<h1>"+getString(R.string.rulesInfo)+"</h1>")));
     }
 }
